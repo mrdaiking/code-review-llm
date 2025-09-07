a582ab2fc374679fe01e36753fd69ba7fd7eb8fd
mrdaiking <daikynguyen999@gmail.com>
2025-09-07 12:06:42 +0900

    Test message send to slack

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,9 @@ function App() {
       <p className="read-the-docs">
         Change Github Action flow and test second commit
       </p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more about Vite and React
+      </p>
     </>
   )
 }
